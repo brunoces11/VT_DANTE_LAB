dc116e5fa02844398de9434c9bd187d74e11e94b
brunoces11 <tutorial.master.brasil@gmail.com>
2025-09-17 05:04:02 -0300

    Updated input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "../../services/util_tailwind"
+import { cn } from "@/lib/utils"
 
 interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
