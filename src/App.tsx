9e1ee409dc70476f6ff0f5be8e6caca7239744b2
brunoces11 <tutorial.master.brasil@gmail.com>
2025-09-18 20:00:37 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,20 @@ import Contato from '@/pages/Contato';
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
+    <AuthProvider>
+      <Router>
+        <Header />
         <Routes>
-          <Route path="/" element={<><Header /><HomePage /></>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/chat-page" element={<ChatPage />} />
-          <Route path="/dante-ui" element={<><Header /><DanteUI /></>} />
-          <Route path="/base-legal" element={<><Header /><BaseLegal /></>} />
-          <Route path="/como-funciona" element={<><Header /><ComoFunciona /></>} />
-          <Route path="/planos" element={<><Header /><Planos /></>} />
-          <Route path="/contato" element={<><Header /><Contato /></>} />
+          <Route path="/dante-ui" element={<DanteUI />} />
+          <Route path="/base-legal" element={<BaseLegal />} />
+          <Route path="/como-funciona" element={<ComoFunciona />} />
+          <Route path="/planos" element={<Planos />} />
+          <Route path="/contato" element={<Contato />} />
         </Routes>
-      </AuthProvider>
-    </Router>
+      </Router>
+    </AuthProvider>
   );
 }
 
