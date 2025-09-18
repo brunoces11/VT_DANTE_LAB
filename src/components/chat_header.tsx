553bed99cb1028287b3330a75647883b7cf649ea
brunoces11 <tutorial.master.brasil@gmail.com>
2025-09-18 19:52:17 -0300

    Garantir que ChatPage só mostre o ChatHeader

--- src/components/chat_header.tsx
+++ src/components/chat_header.tsx
@@ -46,7 +46,7 @@ export default function ChatHeader() {
 
   return (
     <>
-      <header className="fixed top-0 left-0 right-0 z-50 max-h-[60px] h-[60px] w-full bg-gray-100 border-b border-gray-200">
+      <header className="fixed top-0 left-0 right-0 z-[9999] max-h-[60px] h-[60px] w-full bg-gray-100 border-b border-gray-200">
         <div className="flex items-center justify-between h-full pl-4 pr-6">
         {/* Logo no canto esquerdo */}
         <button 
