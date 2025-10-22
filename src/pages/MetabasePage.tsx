d06bc9d976b8b8812cc78349191b55e25c34e208
Bruno Cesar <alecrim.bruno@gmail.com>
2025-10-22 14:34:50 -0300

    Ajuste fino iframe Monitor metabase

--- src/pages/MetabasePage.tsx
+++ src/pages/MetabasePage.tsx
@@ -6,11 +6,13 @@ export default function MetabasePage() {
       <Header />
 
       {/* Iframe Container - com margin-top para não sobrepor o header */}
-      <main className="flex-1 w-full mt-[100px]" style={{ height: 'calc(100vh - 164px)' }}>
+      <main className="flex-1 w-full mt-[59px]" style={{ height: 'calc(100vh - 123px)' }}>
         <iframe
           src="http://meta.prompt-master.org/public/dashboard/3dfc519b-4b32-4bc7-b489-e9fd2d4b7c57"
           frameBorder="0"
           allowTransparency
+          sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+          allow="fullscreen"
           className="w-full h-full border-0"
           title="Metabase Dashboard"
         />
