971216ccfd4953209014a6c27b467ab0d481b8da
brunoces11 <tutorial.master.brasil@gmail.com>
2025-11-14 23:32:50 -0300

    Updated chat_header.tsx

--- src/components/chat_header.tsx
+++ src/components/chat_header.tsx
@@ -25,7 +25,7 @@ export default function ChatHeader() {
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 max-h-[60px] h-[60px] w-full bg-muted border-b border-border">
-      <div className="flex items-center justify-between h-full pl-4 pr-6">
+      <div className="flex items-center justify-between h-full pr-6">
         {/* Logo no canto esquerdo */}
         <button
           onClick={() => navigate('/')}
